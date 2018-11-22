cd6ee09a7676d5355e2789523b9db7e4e44d5663
M <szumagda@wp.pl>
2018-11-22 19:55:23 +0100

    initial commit

---
+++ src/main/java/com/sda/javapoz11/hellospring/hellospring/service/UsersService.java
@@ -0,0 +1,40 @@
+package com.sda.javapoz11.hellospring.hellospring.service;
+
+import com.sda.javapoz11.hellospring.hellospring.entity.User;
+import com.sda.javapoz11.hellospring.hellospring.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UsersService {
+    @Autowired
+    private UsersRepository usersRepository;
+
+    public User findById(long id) {
+        return usersRepository.findById(id)
+                .orElse(null);
+    }
+
+    public List<User> findAll() {
+        List<User> users = new ArrayList<>();
+        usersRepository.findAll()
+                .forEach(e->users.add(e));
+        return users;
+    }
+
+    public List<User> findByLastName(String lastName) {
+        return usersRepository.findByLastName(lastName);
+    }
+
+    @PostConstruct
+    private void init() {
+        usersRepository.save(User.builder().firstName("Harry").lastName("Potter").age(24).build());
+        usersRepository.save(User.builder().firstName("Hermiona").lastName("Granger").age(25).build());
+        usersRepository.save(User.builder().firstName("Ron").lastName("Weasley").age(23).build());
+        usersRepository.save(User.builder().firstName("Fred").lastName("Weasley").age(33).build());
+    }
+}
